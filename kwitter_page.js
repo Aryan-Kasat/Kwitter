b9f7ed3cba0070861c80cb5b6212d7b44f037d15
Aryan-Kasat <72985192+Aryan-Kasat@users.noreply.github.com>
2021-05-31 19:43:16 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -29,10 +29,28 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+           console.log(firebase_message_id);
+           console.log(message_data);
+           names=message_data['name'];
+           message=message_data['message'];
+           like=message_data['like'];
+           name_tag="<h4>"+names+"<img src='tick.png' class='user_tick'></h4>";
+           message_tag="<h4 class='message_h4'>"+message+"</h4>";
+           button_tag="<button class='btn btn-warning' value="+like+" id="+firebase_message_id+" onclick='updatelike(this.id)'>";
+           span_tag="<span class='glyphicon glyphicon-thumbs-up'> Like: "+like+"</span> </button>";
+           row=name_tag+message_tag+button_tag+span_tag;
+           document.getElementById("output").innerHTML+=row;
 //End code
       } });  }); }
 getData();
+
+function updatelike(message_id){
+      likes=document.getElementById(message_id).value;
+      update_like=Number(likes)+1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like:update_like
+      });
+}
 function logout(){
 localStorage.removeItem("username_key");
 localStorage.removeItem("roomname_key");
